d155fcbfc21de6ce60612e6c26abc2432a4ce458
nazarashvili1 <nikanazarashvili127@gmail.com>
2024-03-07 09:55:04 -0800

    commit

---
+++ day4.py
@@ -0,0 +1,9 @@
+num1 = int(input("please enter first number: "))
+num2 = int(input("please enter second number:"))
+
+print(type(num1))
+print(type(num2))
+
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
